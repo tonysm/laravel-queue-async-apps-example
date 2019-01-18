43fb460a4a782aaddba4f18b8ef77c2270614163
Tony Messias <tonyzrp@gmail.com>
2019-01-18 20:39:02 -0300

    Add seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(\App\User::class)->create([
+            'name' => 'Tony Messias',
+            'email' => 'tony@madewithlove.be',
+        ]);
     }
 }
